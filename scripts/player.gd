8fcf803fad58abb6bd2dc7de8ccb0989ffb767e2
jakemeinershagen <jakemeinershagen@gmail.com>
2024-12-04 06:21:53 -0600

    work on player controller

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-
+const ROTATION_SPEED = 1.0
+const ACCEL = 2.0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -9,17 +10,16 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		velocity = Vector2.ZERO
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	velocity += ACCEL * Input.get_action_strength("burn") * -transform.y
+	print(velocity)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("roll_left", "roll_right")
 	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		rotation += direction * ROTATION_SPEED * delta
 
 	move_and_slide()
